e85bb2e95d969959e857120bbaf24cc4a3a90e4f
harimkims <harimkims@gmail.com>
2023-01-29 01:20:22 +0900

    chore: 안쓰는 패키지 제거

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 const nextJest = require('next/jest')
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
