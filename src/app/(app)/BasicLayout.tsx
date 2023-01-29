05fdce7f8f2a4e2690c8c80c94e23798eaa230dd
harimkims <harimkims@gmail.com>
2023-01-29 13:34:31 +0900

    feat: 헤더 메뉴 잘리는 부분 수정

--- src/app/(app)/BasicLayout.tsx
+++ src/app/(app)/BasicLayout.tsx
@@ -30,6 +30,8 @@ const BasicLayout = ({ children }: BasicLayoutProps) => {
     router.push('/')
   }
 
+  console.log(data)
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Header style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -54,6 +56,7 @@ const BasicLayout = ({ children }: BasicLayoutProps) => {
               ],
             },
           ]}
+          style={{ flex: 1, justifyContent: 'flex-end' }}
         />
       </Header>
       <Content style={{ padding: '0 50px' }}>
